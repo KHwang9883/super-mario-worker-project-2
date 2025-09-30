09a30499ae740125be16a62848f5fba9d2bf0bee
GreenSweet233 <2382028511@qq.com>
2025-09-30 14:27:37 +0800

    进一步精确复刻 GM 版 Mario 的基础运动

--- script/level/player/Mario.cs
+++ script/level/player/Mario.cs
@@ -3,93 +3,100 @@ using System;
 
 public partial class Mario : CharacterBody2D
 {
-    private float speed = 0.0f;
-    private readonly float walkingAcceleration = 0.1f * 50.0f;
-    private readonly float runningAcceleration = 0.3f * 50.0f;
-    private readonly float maxWalkingSpeed = 3.0f * 50.0f;
-    private readonly float maxRunningSpeed = 8.0f * 50.0f;
-    private int direction = 1;
+    private float _speed = 0.0f;
+    private float _walkingAcceleration = 0.1f * 50.0f;
+    private float _runningAcceleration = 0.3f * 50.0f;
+    private float _maxWalkingSpeed = 3.0f * 50.0f;
+    private float _maxRunningSpeed = 8.0f * 50.0f;
+    private int _direction = 1;
 
-    private readonly float gravity = 1.0f * 50.0f;
-    private readonly float maxFallingSpeed = 13.0f * 50.0f;
-    private float jumpBoostTimer = 0.0f;
+    public float Gravity = 5.0f / 5.0f * 50.0f;
+    private float _maxFallingSpeed = 13.0f * 50.0f;
 
-    private bool inWater = false;
+    private bool _doubleFrameProcess = false;
+    //private float jumpBoostTimer = 0.0f;
 
-    private bool up;
-    private bool down;
-    private bool left;
-    private bool right;
-    private bool fire;
-    private bool jump;
+    private bool _inWater = false;
 
-    public override void _PhysicsProcess(double delta)
-    {
+    private bool _up;
+    private bool _down;
+    private bool _left;
+    private bool _right;
+    private bool _fire;
+    private bool _jump;
+
+    public override void _PhysicsProcess(double delta) {
         // 输入处理
-        up = Input.IsActionPressed("move_up");
-        down = Input.IsActionPressed("move_down");
-        left = Input.IsActionPressed("move_left");
-        right = Input.IsActionPressed("move_right");
-        fire = Input.IsActionPressed("move_fire");
-        jump = Input.IsActionPressed("move_jump");
+        _up = Input.IsActionPressed("move_up");
+        _down = Input.IsActionPressed("move_down");
+        _left = Input.IsActionPressed("move_left");
+        _right = Input.IsActionPressed("move_right");
+        _fire = Input.IsActionPressed("move_fire");
+        _jump = Input.IsActionPressed("move_jump");
             
         // x 速度
-        if (!fire)
-        {
-            if (left)
-            {
-                speed = Mathf.Clamp(speed - walkingAcceleration, -maxWalkingSpeed, maxWalkingSpeed);
+        if (!_fire) {
+            if (_left) {
+                _speed = Mathf.Clamp(_speed - _walkingAcceleration, -_maxWalkingSpeed, _maxWalkingSpeed);
             }
-            if (right)
-            {
-                speed = Mathf.Clamp(speed + walkingAcceleration, -maxWalkingSpeed, maxWalkingSpeed);
+            if (_right) {
+                _speed = Mathf.Clamp(_speed + _walkingAcceleration, -_maxWalkingSpeed, _maxWalkingSpeed);
             }
-        }
-        else
-        {
-            if (left)
-            {
-                speed = Mathf.Clamp(speed - runningAcceleration, -maxRunningSpeed, maxRunningSpeed);
+        } else {
+            if (_left) {
+                _speed = Mathf.Clamp(_speed - _runningAcceleration, -_maxRunningSpeed, _maxRunningSpeed);
             }
-            if (right)
-            {
-                speed = Mathf.Clamp(speed + runningAcceleration, -maxRunningSpeed, maxRunningSpeed);
+            if (_right) {
+                _speed = Mathf.Clamp(_speed + _runningAcceleration, -_maxRunningSpeed, _maxRunningSpeed);
             }
         }
-            
-        if (!left && !right)
-        {
-            speed /= 1.05f;
+        if (!_left && !_right) {
+            _speed /= 1.05f;
         }
-            
-        if (speed > -0.04f && speed < 0.04f)
-        {
-            speed = 0.0f;
+        if (_speed > -0.04f && _speed < 0.04f) {
+            _speed = 0.0f;
         }
-            
-        Velocity = new Vector2(speed, Velocity.Y);
+        
+        Velocity = new Vector2(_speed, Velocity.Y);
             
         // y 速度
-        if (IsOnFloor() && jump)
-        {
-            Velocity = new Vector2(Velocity.X, -(8.0f + Mathf.Abs(speed / 50.0f) / 5.0f) * 50.0f);
+        if (IsOnFloor()) {
+            _doubleFrameProcess = true;
+        }
+        
+        if (IsOnFloor() && _jump) {
+            Velocity = new Vector2(Velocity.X, -(8.0f + Mathf.Abs(_speed / 50.0f) / 5.0f) * 50.0f);
         }
             
-        if (jump && Velocity.Y < 0 && !inWater)
-        {
-            Velocity = new Vector2(Velocity.X, Velocity.Y - 0.78f * 50.0f);
+        if (_jump && Velocity.Y < 0 && !_inWater && _doubleFrameProcess) {
+            Velocity = new Vector2(Velocity.X, Velocity.Y - 1.5f * 50.0f);
+        }
+        
+        _doubleFrameProcess = !_doubleFrameProcess;
+        
+        if (!_inWater) {
+            Velocity = new Vector2(Velocity.X, Velocity.Y + Gravity);
         }
             
-        if (!inWater)
-        {
-            Velocity = new Vector2(Velocity.X, Velocity.Y + gravity);
+        if (!IsOnFloor() && Velocity.Y > _maxFallingSpeed) {
+            Velocity = new Vector2(Velocity.X, _maxFallingSpeed);
         }
-            
-        if (!IsOnFloor() && Velocity.Y > maxFallingSpeed)
-        {
-            Velocity = new Vector2(Velocity.X, maxFallingSpeed);
+
+        if (IsOnCeiling() && Velocity.Y < 0) {
+            Velocity = new Vector2(Velocity.X, 0);
         }
-            
+        
         MoveAndSlide();
+        
+        // GM8版注释：尝试性修复非整格实心穿墙
+        // 为保持精确性，故撞墙、撞天花板和落地进行一次取整
+        if (IsOnWall()) {
+            Position = new Vector2(Mathf.Round(Position.X), Position.Y);
+        }
+        if (IsOnFloor() || IsOnCeiling()) {
+            Position = new Vector2(Position.X, Mathf.Round(Position.Y));
+        }
     }
+
+    
 }
