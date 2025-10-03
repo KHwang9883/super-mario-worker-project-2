2abd33bd522345c263b1ebd07d0c9e16d761fcf4
GreenSweet233 <2382028511@qq.com>
2025-10-03 14:25:58 +0800

    整理水域及其代码
    基础内容（水块跟随水面）已完善，水位的运动待做

--- script/level/water/Water.cs
+++ script/level/water/Water.cs
@@ -2,22 +2,23 @@ using Godot;
 using System;
 
 public partial class Water : Area2D {
+    [Export] public Resource waterBoundary { get; private set; } = null!;
     [Export] private Parallax2D _parallaxWater = null!;
     [Export] private AnimatedSprite2D _waterSurfaceSprite = null!;
     [Export] private ColorRect _waterRect = null!;
     [Export] private Camera2D _camera = null!;
 
-    private double _testWaterPhase = 233f;
+    // 测试水块跟随用，可以删除
+    //private double _testWaterPhase = 233f;
 
     public override void _Process(double delta) {
-        // 获取当前动画名称和当前帧索引
+        
         string currentAnim = _waterSurfaceSprite.Animation;
         int currentFrame = _waterSurfaceSprite.Frame;
         
-        // 使用当前动画和帧索引获取纹理
         Texture2D currentTexture = _waterSurfaceSprite.GetSpriteFrames().GetFrameTexture(currentAnim, currentFrame);
         
-        // 计算水面位置
+        // 计算水块位置
         _waterRect.GlobalPosition = new Vector2(
             _waterRect.GlobalPosition.X, 
             (float)Math.Max(
@@ -26,8 +27,8 @@ public partial class Water : Area2D {
             )
         );
         
-        // 水面波动效果
-        Position = new Vector2(Position.X, 233 + (float)Math.Sin(_testWaterPhase) * 128);
-        _testWaterPhase += delta * 2f;
+        // 测试水块跟随用，可以删除
+        //Position = new Vector2(Position.X, 233 + (float)Math.Sin(_testWaterPhase) * 128);
+        //_testWaterPhase += delta * 2f;
     }
 }
