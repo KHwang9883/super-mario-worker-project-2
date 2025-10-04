76a5e3a58076f8c105a7e159cf5fdba5d5417ba9
GreenSweet233 <2382028511@qq.com>
2025-10-04 15:30:35 +0800

    完成地面上运动的手感，调整为原版的执行顺序
    已知问题：
    - 在顶天花板边缘会被吸住一段时间
    - 水面与地面重合的时候可能触发y速度很小的连跳
    - 水面上跳跃高度比原版大

--- script/level/player/Mario.cs
+++ script/level/player/Mario.cs
@@ -4,30 +4,29 @@ using System;
 public partial class Mario : CharacterBody2D {
     [Export] private Area2D _waterDetect = null!;
 
-    public float Gravity = 5.0f / 5.0f;
+    // 关卡引力
+    private float _gravity = 5f / 5f;
     
+    // 运动参数先用 GM8 单位（px/f），计算 Velocity 的时候转换为 Godot 单位（px/s）
+    private const float FRAMERATE_ORIGIN = 50f;
     private int _direction = 1;
     private float _speedX;
-    private float _walkingAcceleration = 0.1f * 2500.0f;
-    private float _runningAcceleration = 0.3f * 2500.0f;
-    private float _maxWalkingSpeed = 3.0f * 50.0f;
-    private float _maxRunningSpeed = 8.0f * 50.0f;
+    private float _walkingAcceleration = 0.1f;
+    private float _runningAcceleration = 0.3f;
+    private float _maxWalkingSpeed = 3f;
+    private float _maxRunningSpeed = 8f;
     
-    //private float _speedY;
-    private float _maxFallingSpeed = 13.0f * 50.0f;
-
-    private Resource _waterBoundary = null!;
+    private float _speedY;
+    private float _maxFallingSpeed = 13f;
     
-    private float _waterHorizontalAcceleration = 0.05f * 2500.0f;
-    private float _waterMaxWalkingSpeed = 1.0f * 50.0f;
-    private float _waterMaxRunningSpeed = 3.0f * 50.0f;
-    private float _waterMaxFallingSpeed = 6.0f * 50.0f;
-
-    private bool _doubleFrameProcess;
-    //private float jumpBoostTimer = 0.0f;
-
+    private float _waterHorizontalAcceleration = 0.05f;
+    private float _waterMaxWalkingSpeed = 1f;
+    private float _waterMaxRunningSpeed = 3f;
+    private float _waterMaxFallingSpeed = 6f;
+    
     public bool InWater;
-    public bool OnWaterSurface;
+    public bool OnWaterSurface = true;
+    private Area2D _waterArea;
 
     private bool _up;
     private bool _down;
@@ -36,9 +35,12 @@ public partial class Mario : CharacterBody2D {
     private bool _fire;
     private bool _jump;
     private bool _jumped;
-
+    private int _jumpBoostTimer;
+    //private bool _doubleFrameProcess;
+    //private bool _lastJumpBoostState;
     public override void _Ready() {
         Area2D waterArea = GetTree().GetFirstNodeInGroup("water") as Area2D;
+        _waterArea = waterArea;
         
         if (waterArea == null) {
             GD.PrintErr("Water Not Found.");
@@ -47,14 +49,17 @@ public partial class Mario : CharacterBody2D {
         
         waterArea.BodyEntered += OnWaterBodyEntered;
         waterArea.BodyExited += OnWaterBodyExited;
+        
+        _waterDetect.AreaEntered += OnWaterBodyEntered;
+        _waterDetect.AreaExited += OnWaterBodyExited;
     }
 
     private void OnWaterBodyEntered(Node2D body) {
         if (body == this) {
             InWater = true;
-            Velocity = new Vector2(Velocity.X, 0.0f);
+            _speedY = 0f;
         }
-        if (body == _waterDetect) {
+        if (body == _waterArea) {
             OnWaterSurface = false;
         }
     }
@@ -63,10 +68,11 @@ public partial class Mario : CharacterBody2D {
         if (body == this) {
             InWater = false;
         }
-        if (body == _waterDetect) {
+        if (body == _waterArea) {
             OnWaterSurface = true;
         }
     }
+    
     public override void _PhysicsProcess(double delta) {
         // 输入处理
         _up = Input.IsActionPressed("move_up");
@@ -81,15 +87,25 @@ public partial class Mario : CharacterBody2D {
             ? _waterHorizontalAcceleration
             : (_fire ? _runningAcceleration : _walkingAcceleration);
 
-        float maxSpeed = InWater 
-            ? (_fire ? _waterMaxRunningSpeed : _waterMaxWalkingSpeed)
-            : (_fire ? _maxRunningSpeed : _maxWalkingSpeed);
+        float maxSpeed;
+        
+        if (InWater) {
+            maxSpeed = _fire ? _waterMaxRunningSpeed : _waterMaxWalkingSpeed;
+        } else {
+            if (_fire) {
+                maxSpeed = _maxRunningSpeed;
+            } else {
+                maxSpeed = Mathf.Abs(_speedX) > _maxWalkingSpeed 
+                    ? _maxRunningSpeed 
+                    : _maxWalkingSpeed;
+            }
+        }
 
         if (_left) {
-            _speedX = Mathf.Clamp(_speedX - acceleration * (float)delta, -maxSpeed, maxSpeed);
+            _speedX = Mathf.Clamp(_speedX - acceleration, -maxSpeed, maxSpeed);
         }
         if (_right) {
-            _speedX = Mathf.Clamp(_speedX + acceleration * (float)delta, -maxSpeed, maxSpeed);
+            _speedX = Mathf.Clamp(_speedX + acceleration, -maxSpeed, maxSpeed);
         }
 
         if (!_left && !_right) {
@@ -97,67 +113,67 @@ public partial class Mario : CharacterBody2D {
         }
         
         if (_speedX > -0.04f && _speedX < 0.04f) {
-            _speedX = 0.0f;
+            _speedX = 0f;
         }
-        
-        Velocity = new Vector2(_speedX, Velocity.Y);
             
         // y 速度
         // 起跳
-        if (IsOnFloor()) {
-            _doubleFrameProcess = true;
-        }
         
         if (!_jump) { _jumped = false; }
         
         if (_jump) {
             if (!InWater && IsOnFloor()) {
-                Velocity = new Vector2(Velocity.X, -(8.0f + Mathf.Abs(_speedX / 50.0f) / 5.0f) * 50.0f);
+                _speedY = -(8f + Mathf.Abs(_speedX) / 5f);
+                _jumped = true;
             } else if (InWater && !_jumped) {
-                Velocity = new Vector2(
-                    Velocity.X,
-                    OnWaterSurface
-                        ? -(6.0f + Mathf.Abs(_speedX / 50.0f) / 5.0f) * 50.0f
-                        : -(4.0f + Mathf.Abs(_speedX / 50.0f) / 10.0f) * 50.0f
-                );
+                _speedY = OnWaterSurface
+                    ? -(6f + Mathf.Abs(_speedX) / 5f)
+                    : -(4f + Mathf.Abs(_speedX) / 10f);
                 _jumped = true;
             }
         }
         
         // 空中跳跃按跳跃键有速度加成
-        if (_jump && Velocity.Y < 0 && !InWater && _doubleFrameProcess) {
-            Velocity = new Vector2(Velocity.X, Velocity.Y - 1.5f * 2500.0f * (float)delta);
+        _jumpBoostTimer = Math.Clamp(_jumpBoostTimer + 1, 0, 2);
+
+        if (_jump && _speedY < 0f && !InWater) {
+            if (_jumpBoostTimer > 1) {
+                _speedY -= 1.5f;
+                _jumpBoostTimer = 0;
+            }
         }
-        
-        _doubleFrameProcess = !_doubleFrameProcess;
-        
-        // 重力
-        Velocity = new Vector2(Velocity.X,
-            Velocity.Y + (InWater ? 0.2f : 1.0f) * 2500.0f * (float)delta + Gravity
-        );
         
         // 最大下落速度
         if (!IsOnFloor()) {
             float maxFallSpeed = InWater ? _waterMaxFallingSpeed : _maxFallingSpeed;
     
-            if (Velocity.Y > maxFallSpeed) {
-                Velocity = new Vector2(Velocity.X, maxFallSpeed);
+            if (_speedY > maxFallSpeed) {
+                _speedY = maxFallSpeed;
             }
         }
-        
-        // 顶头
-        if (IsOnCeiling() && Velocity.Y < 0) {
-            Velocity = new Vector2(Velocity.X, 0.0f);
-        }
+
+        // 根据GM8版执行顺序在这里进行 MoveAndSlide()
+        Velocity = new Vector2(_speedX * FRAMERATE_ORIGIN, (_speedY + _gravity) * FRAMERATE_ORIGIN);
         
         MoveAndSlide();
         
+        // 重力
+        _speedY += (InWater ? 0.2f : 1.0f);
+        
+        // 落地或顶头
+        if ((IsOnFloor() || (IsOnCeiling() && _speedY < 0f))) {
+            _speedY = 0f;
+            /*if (InWater) {
+                _jumped = true;
+            }*/
+        }
+        
         // GM8版注释：尝试性修复非整格实心穿墙
-        // 为保持精确性，故撞墙、撞天花板和落地进行一次取整
-        if (IsOnWall()) {
+        // 为保持精确性，故各自方向速度为零分别进行一次取整
+        if (_speedX == 0f) {
             Position = new Vector2(Mathf.Round(Position.X), Position.Y);
         }
-        if (IsOnFloor() || IsOnCeiling()) {
+        if (_speedY == 0f) {
             Position = new Vector2(Position.X, Mathf.Round(Position.Y));
         }
     }
