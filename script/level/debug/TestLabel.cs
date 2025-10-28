5a91f2c6b17206c4bcf6f64ede2e02035db1779b
GreenSweet233 <2382028511@qq.com>
2025-10-28 21:55:06 +0800

    Update TestLabel.cs

--- script/level/debug/TestLabel.cs
+++ script/level/debug/TestLabel.cs
@@ -1,21 +1,31 @@
 using Godot;
+using SMWP.Level.Player;
 
 namespace SMWP.Level.Debug;
 
 public partial class TestLabel : Label {
-    [Export] private PMovement _player = null!;
-    
+    [Export] private CharacterBody2D _player = null!;
+
+    private PlayerMovement _playerMovement = null!;
+
+    public override void _Ready() {
+        _playerMovement = _player.GetNode<Node>("PlayerMediator").GetNode<PlayerMovement>("PlayerMovement");
+    }
+
     public override void _PhysicsProcess(double delta) {
         Print();
     }
 
     public void Print() {
+        if ((_player.ProcessMode == ProcessModeEnum.Disabled) || (_player == null)) {
+            return;
+        }
         Text = $"Position: ({_player.Position.X,9:00000.00}, {_player.Position.Y,9:00000.00})\n" +
                $"Velocity: ({_player.Velocity.X,7:000.00}, {_player.Velocity.Y,7:000.00})\n" +
                $"IsOnFloor: {_player.IsOnFloor()}\n" +
                $"IsOnCeiling: {_player.IsOnCeiling()}\n" +
                $"IsOnWall: {_player.IsOnWall()}\n" +
-               $"InWater: {_player.IsInWater}\n" +
-               $"OnWaterSurface: {_player.IsOnWaterSurface}";
+               $"InWater: {_playerMovement.IsInWater}\n" +
+               $"OnWaterSurface: {_playerMovement.IsOnWaterSurface}";
     }
 }
