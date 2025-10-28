a3305482f974b2a8403ce1f73568a657e2bf0858
GreenSweet233 <2382028511@qq.com>
2025-10-28 21:53:12 +0800

    增加工具类用于获取屏幕位置

---
+++ script/tool/ScreenUtils.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public static class ScreenUtils {
+    public static Rect2 GetScreenRect(Node node) {
+        Viewport viewport = node.GetViewport();
+        Rect2 viewportRect = viewport.GetVisibleRect();
+        Transform2D canvasTransform = viewport.CanvasTransform;
+        Transform2D inverseTransform = canvasTransform.AffineInverse();
+        Rect2 result = new Rect2(inverseTransform * viewportRect.Position, viewportRect.Size);
+        return result;
+    }
+}
