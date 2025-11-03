7127b9ac09daa825a29b0bfc0b981359c26b5bb6
GreenSweet233 <2382028511@qq.com>
2025-11-03 22:30:08 +0800

    尝试性加入玩家阴影特效（参数未成功调整）

---
+++ script/level/player/PlayerEffect.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class PlayerEffect : Sprite2D {
+    public override void _PhysicsProcess(double delta) {
+        Modulate = Modulate with { A = Modulate.A - 0.2f };
+        if (Modulate.A <= 0f) {
+            QueueFree();
+        }
+    }
+}
