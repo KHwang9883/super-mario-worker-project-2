42d55b8f3b5a04d405285d6cedcd6598d6e97ba7
GreenSweet233 <2382028511@qq.com>
2025-11-10 13:48:29 +0800

    RoomConfig 改名为 LevelConfig，存储更多关卡信息

---
+++ script/level/LevelConfig.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+using SMWP.Level.Score;
+
+namespace SMWP.Level;
+
+[GlobalClass]
+public partial class LevelConfig : Node {
+    [Export] public float RoomWidth = 1024f;
+    [Export] public float RoomHeight = 768f;
+    [Export] public int Time = 600;
+
+    public override void _Ready() {
+        ScoreManager.Time = Time;
+    }
+}
--- script/level/RoomConfig.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-namespace SMWP.Level;
-
-public partial class RoomConfig : Node {
-    [Export] public float RoomWidth = 1024f;
-    [Export] public float RoomHeight = 768f;
-}
