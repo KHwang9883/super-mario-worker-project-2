e1055255a80e731a0cf055ea89bd395eacf12736
GreenSweet233 <2382028511@qq.com>
2025-11-11 21:21:34 +0800

    接口 IToppable 更名为 IBumpHittable

--- script/level/interface/IBumpHittable.cs
+++ script/level/interface/IBumpHittable.cs
@@ -2,9 +2,9 @@ using Godot;
 
 namespace SMWP.Level.Interface;
 
-public interface IToppable {
-    bool IsToppable { get; set; }
+public interface IBumpHittable {
+    bool IsBumpHittable { get; set; }
     
     public void MetadataInject(Node2D parent);
-    public void OnTopped();
+    public void OnBumped();
 }
