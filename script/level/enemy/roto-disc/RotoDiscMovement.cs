450185858ad4bfc147840818d1887f05df67ca03
GreenSweet233 <2382028511@qq.com>
2025-11-13 22:13:07 +0800

    添加探照灯，修改探照灯相关图层

---
+++ script/level/enemy/roto-disc/RotoDiscMovement.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class RotoDiscMovement : Node {
+    [Export] private PackedScene _rotoDiscCenterScene = null!;
+
+    [Export] private float _radius = 32f;
+    [Export] private float _angle;
+    [Export] private float _speed = 1f;
+
+    private Vector2 _originPosition;
+    private Node2D? _parent;
+
+    public override void _Ready() {
+        _parent = (Node2D)GetParent();
+        _originPosition = _parent.Position;
+        // 添加节点如果在 _Ready 中，必须延迟（Godot 魅力时刻）
+        Callable.From(() => {
+            var rotoDiscCenter = _rotoDiscCenterScene.Instantiate<Sprite2D>();
+            rotoDiscCenter.Position = _originPosition;
+            _parent.AddSibling(rotoDiscCenter);
+        }).CallDeferred();
+    }
+    public override void _PhysicsProcess(double delta) {
+        if (_parent == null) return;
+        _angle = Mathf.Wrap(_angle + _speed, 0f, 360f);
+        _parent.Position =
+            new Vector2(
+                _originPosition.X + Mathf.Sin(Mathf.DegToRad(_angle)) * _radius,
+                _originPosition.Y + Mathf.Cos(Mathf.DegToRad(_angle)) * _radius
+                );
+    }
+}
