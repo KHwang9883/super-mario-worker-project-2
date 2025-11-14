c2c12470c42d51fcc95bc585b4628e6c3c313a3a
GreenSweet233 <2382028511@qq.com>
2025-11-14 22:53:55 +0800

    添加炮台、炮弹（普通）

---
+++ script/level/enemy/banzai_bill/BanzaiBillCannonMovement.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+using SMWP.Level.Tool;
+
+public partial class BanzaiBillCannonMovement : Node {
+    [Signal]
+    public delegate void PlaySoundBulletEventHandler();
+
+    [Export] private float _triggerDistance = 75f;
+    [Export] private PackedScene _banzaiBillScene = null!;
+    [Export] private float _shootTime = 200f;
+    [Export] private float _shootTimeBonus = 0.1f;
+    private Node2D? _parent;
+    private Node2D? _player;
+    private float _timer;
+    private float _timeAddSpeedImproved;
+    private RandomNumberGenerator _rng = new RandomNumberGenerator();
+
+    public override void _Ready() {
+        _parent ??= GetParent<Node2D>();
+        _player ??= (Node2D)GetTree().GetFirstNodeInGroup("player");
+        _timeAddSpeedImproved = 1f + _rng.RandfRange(0f, _shootTimeBonus);
+    }
+    public override void _PhysicsProcess(double delta) {
+        if (_parent == null || _player == null) return;
+        if ((_player.Position.X < _parent.Position.X + _triggerDistance
+            && _player.Position.X > _parent.Position.X - _triggerDistance)
+            || (_parent.Position.X < ScreenUtils.GetScreenRect(this).Position.X)
+            || (_parent.Position.X > ScreenUtils.GetScreenRect(this).End.X)
+            ) return;
+        _timer += _timeAddSpeedImproved;
+        if (_timer < _shootTime) return;
+        var banzaiBill = (Node2D)_banzaiBillScene.Instantiate();
+        banzaiBill.Position = _parent.Position;
+        _parent.AddSibling(banzaiBill);
+        _timer = 0f;
+        _timeAddSpeedImproved = 1f + _rng.RandfRange(0f, _shootTimeBonus);
+        if (_parent.Position.Y > ScreenUtils.GetScreenRect(this).Position.Y - 16f
+            && _parent.Position.Y < ScreenUtils.GetScreenRect(this).End.Y + 16f) {
+            EmitSignal(SignalName.PlaySoundBullet);
+        }
+    }
+}
