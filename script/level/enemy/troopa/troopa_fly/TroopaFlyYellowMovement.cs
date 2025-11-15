c3dff6e3da316461ebcbafbb0b09155c0fb033f2
GreenSweet233 <2382028511@qq.com>
2025-11-15 20:07:29 +0800

    添加黄飞龟（不含生成器）

---
+++ script/level/enemy/troopa/troopa_fly/TroopaFlyYellowMovement.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using Godot.Collections;
+
+public partial class TroopaFlyYellowMovement : Node {
+    [Export] private float _radius;
+    [Export] private float _angleSpeed;
+    [Export] private float _angle;
+    
+    [Export] private float _dir;
+    
+    private Node2D? _parent;
+    private Vector2 _originPos;
+    
+    public override void _Ready() {
+        _parent ??= (Node2D)GetParent();
+        _originPos = _parent.Position;
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        if (_parent == null) return;
+        
+        _parent.Position =
+            new Vector2(
+                _originPos.X + Mathf.Sin(Mathf.DegToRad(_angle)) *_radius,
+                _originPos.Y + Mathf.Cos(Mathf.DegToRad(_angle)) *_radius
+                );
+        
+        if (_radius <= 150) {
+            _angle -= _dir * 2f - 1f;
+        } else {
+            _angle -= (150f / _radius) * (_dir * 2f - 1f);
+        }
+    }
+}
