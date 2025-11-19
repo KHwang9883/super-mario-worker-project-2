5f553da6e8e3ce58120f366c83ec0800ce4a86b4
GreenSweet233 <2382028511@qq.com>
2025-11-19 18:37:41 +0800

    修复踢静止龟壳和运动龟壳踩停后立刻被踢的问题

--- script/level/enemy/troopa/shell/ShellStaticInteraction.cs
+++ script/level/enemy/troopa/shell/ShellStaticInteraction.cs
@@ -5,6 +5,5 @@ using SMWP.Level.Enemy;
 public partial class ShellStaticInteraction : GoombaEnemyInteraction {
     public override void PlayerHurtCheck(bool check) {
         EmitSignal(GoombaEnemyInteraction.SignalName.GoombaStomped, Variant.From(EnemyDieType));
-        GD.Print(1112);
     }
 }
