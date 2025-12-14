1dba2627e998395837e175403b376dc7e983e83c
GreenSweet233 <2382028511@qq.com>
2025-12-14 12:59:13 +0800

    添加新版光源可调节等级改变半径

--- script/level/weather/light/BrightController.cs
+++ script/level/weather/light/BrightController.cs
@@ -13,13 +13,15 @@ public partial class BrightController : Node {
     public const int MaxLights = 256;   // 重要：与 Shader 中的 MAX_LIGHTS 保持一致
     private static Vector2[] Positions = new Vector2[MaxLights];
     private static int _positionCount = 0;
+    private float _radiusRatio;
     
     public override void _Ready() {
         _levelConfig = LevelConfigAccess.GetLevelConfig(this);
         _darknessMask = (Node2D)GetTree().GetFirstNodeInGroup("darkness_mask");
         _shaderMaterial = (ShaderMaterial)_darknessMask.Material;
     }
-    public override void _Process(double delta) {
+
+    public override void _PhysicsProcess(double delta) {
         if (_levelConfig == null) return;
         BrightLevel = _levelConfig.Brightness;
         if (_darknessMask == null) {
@@ -27,24 +29,36 @@ public partial class BrightController : Node {
             return;
         }
         
-        var screen = ScreenUtils.GetScreenRect(this);
-        var nodes = GetTree().GetNodesInGroup("lights");
+        _radiusRatio = BrightLevel switch {
+            0 => 0.000000001f,
+            1 => 0.25f,
+            2 => 0.5f,
+            3 => 1f,
+            4 => 1.8f,
+            5 => 3f,
+            _ => _radiusRatio,
+        };
 
-        _positionCount = 0;
-        Positions = new Vector2[MaxLights];
-        foreach (var node in nodes) {
-            if (node is not SmwpPointLight2D light) continue;
-            if (!light.Enabled) continue;
-            
-            Positions[_positionCount] = light.GlobalPosition - screen.Position;
-            _positionCount++;
-            // 大于最大光源数就停止遍历
-            if (_positionCount < MaxLights) continue;
+        Callable.From(() => {
+            var screen = ScreenUtils.GetScreenRect(this);
+            var nodes = GetTree().GetNodesInGroup("lights");
+
             _positionCount = 0;
-            break;
-        }
-        
-        // Apply
-        _shaderMaterial?.SetShaderParameter("positions", Positions);
+            Positions = new Vector2[MaxLights];
+            foreach (var node in nodes) {
+                if (node is not SmwpPointLight2D light) continue;
+                if (!light.Enabled) continue;
+            
+                Positions[_positionCount] = light.GlobalPosition - screen.Position;
+                _positionCount++;
+                // 大于最大光源数就停止遍历
+                if (_positionCount < MaxLights) continue;
+                _positionCount = 0;
+                break;
+            }
+            
+            _shaderMaterial?.SetShaderParameter("positions", Positions);
+            _shaderMaterial?.SetShaderParameter("radius_ratio", _radiusRatio);
+        }).CallDeferred();
     }
 }
