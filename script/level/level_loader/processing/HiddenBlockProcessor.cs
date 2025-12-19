0234249f791e554d20ce0070eab3788ae93c7bb1
GreenSweet233 <2382028511@qq.com>
2025-12-19 09:10:51 +0800

    添加 dusk_tree；更新启动图标；添加景物数据库内容
    还有敌人、marks db；
    更正游鱼/飞鱼控件命名。

--- script/level/level_loader/processing/HiddenBlockProcessor.cs
+++ script/level/level_loader/processing/HiddenBlockProcessor.cs
@@ -4,7 +4,7 @@ using SMWP.Util;
 namespace SMWP.Level.Loader.Processing;
 
 /// <summary>
-/// 让问好块变成隐藏块
+/// 让问号块变成隐藏块
 /// </summary>
 public partial class HiddenBlockProcessor : ObjectProcessor {
     public override void ProcessObject(Node instance, string metadata) {
