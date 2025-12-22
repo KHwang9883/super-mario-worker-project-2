23e42f19a907856806953881e7a314eab14ad11e
GreenSweet233 <2382028511@qq.com>
2025-12-22 12:37:13 +0800

    修复蓝跳龟跳跃高度读取；添加蓝跳龟顶头处理

--- script/level/enemy/troopa/troopa_fly/TroopaFlyBlueMovement.cs
+++ script/level/enemy/troopa/troopa_fly/TroopaFlyBlueMovement.cs
@@ -5,6 +5,14 @@ using SMWP.Level.Physics;
 public partial class TroopaFlyBlueMovement : BasicMovement {
     [Export] public float Height = 32f;
 
+    public override void _PhysicsProcess(double delta) {
+        base._PhysicsProcess(delta);
+        // 顶头 y 速度取绝对值
+        if (MoveObject.IsOnCeiling()) {
+            SpeedY = Mathf.Abs(SpeedY);
+        }
+    }
+
     public override void SetJumpSpeed() {
         if (!MoveObject.IsOnFloor()) return;
         //SpeedY = Mathf.Min(0f, -(25f / Mathf.Pow(Height, 3) - 10f / Mathf.Pow(Height, 2)));
--- script/level/level_loader/processing/SetJumpHeightMetaProcessor.cs
+++ script/level/level_loader/processing/SetJumpHeightMetaProcessor.cs
@@ -9,8 +9,8 @@ public partial class SetJumpHeightMetaProcessor : ObjectProcessor {
     public override void ProcessObject(Node node, ClassicSmwlObject instance) {
         var metadata = instance.Metadata;
         if (float.TryParse(metadata, out var jumpHeight)) {
-            if (node.TryGetComponent(out BasicMovement? movement)) {
-                movement.JumpSpeed = -jumpHeight;
+            if (node.TryGetComponent(out TroopaFlyBlueMovement? movement)) {
+                movement.Height = jumpHeight;
             }
         }
     }
