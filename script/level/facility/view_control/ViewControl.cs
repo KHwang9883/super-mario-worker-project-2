73c20d821ba3fea25c8858a9357d1845f6ce1d5f
GreenSweet233 <2382028511@qq.com>
2025-12-23 12:33:06 +0800

    添加 AutoScroll 和 ViewControl 编辑器界面的显示

--- script/level/facility/view_control/ViewControl.cs
+++ script/level/facility/view_control/ViewControl.cs
@@ -1,11 +1,36 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class ViewControl : Node2D {
     [Export] public Rect2 ViewRect;
+    
+    [Export] private Line2D? _line2D;
+    [Export] private Sprite2D? _topLeft;
+    [Export] private Sprite2D? _bottomRight;
 
     private SceneControl? _sceneControl;
 
+    public override void _Ready() {
+        Visible = Engine.IsEditorHint();
+    }
+    public override void _PhysicsProcess(double delta) {
+        if (Engine.IsEditorHint()) {
+            if (_line2D != null) {
+                _line2D.Points = [
+                    ViewRect.Position,
+                    new Vector2(ViewRect.Position.X + ViewRect.Size.X, ViewRect.Position.Y),
+                    ViewRect.Position + ViewRect.Size,
+                    new Vector2(ViewRect.Position.X, ViewRect.Position.Y + ViewRect.Size.Y),
+                ];
+            }
+            if (_topLeft != null && _bottomRight != null) {
+                _topLeft.GlobalPosition = ViewRect.Position;
+                _bottomRight.GlobalPosition = ViewRect.Position + ViewRect.Size;
+            }
+        }
+    }
+    
     public void SetSceneControl(SceneControl sceneControl) {
         _sceneControl = sceneControl;
     }
