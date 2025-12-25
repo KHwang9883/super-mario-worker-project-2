0767cff1a83c56d042037479ae5f7ac93caf7402
GreenSweet233 <2382028511@qq.com>
2025-12-25 20:21:12 +0800

    微调图层

--- script/level/tile/BlockTileLayer.cs
+++ script/level/tile/BlockTileLayer.cs
@@ -1,19 +1,40 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using Godot.Collections;
 using SMWP.Level;
 
 public partial class BlockTileLayer : Node {
+    [Export] private SmwlLevel? _smwlLevel;
     [Export] private Node2D _blockTile = null!;
-    [Export] private int _targetZIndex = 350;
+    [Export] private GDC.Dictionary<LevelConfig.LayerOrderEnum, int> _targetZIndex = null!;
     
     private LevelConfig? _levelConfig;
 
     public override void _Ready() {
+        if (_smwlLevel != null) {
+            _smwlLevel.LevelLoaded += SetZIndex;
+        } else {
+            SetZIndex();
+        }
+    }
+
+    public void SetZIndex() {
         Callable.From(() => {
             _levelConfig ??= LevelConfigAccess.GetLevelConfig(this);
-            if (_levelConfig.RotoDiscLayer) {
-                _blockTile.ZIndex = _targetZIndex;
-            }
+            
+            _blockTile.ZIndex = _levelConfig.LayerOrder switch {
+                LevelConfig.LayerOrderEnum.Classic =>
+                    _targetZIndex.GetValueOrDefault(LevelConfig.LayerOrderEnum.Classic, _blockTile.ZIndex),
+                
+                LevelConfig.LayerOrderEnum.WaterAbove =>
+                    _targetZIndex.GetValueOrDefault(LevelConfig.LayerOrderEnum.WaterAbove, _blockTile.ZIndex),
+                
+                LevelConfig.LayerOrderEnum.Modified =>
+                    _targetZIndex.GetValueOrDefault(LevelConfig.LayerOrderEnum.Modified, _blockTile.ZIndex),
+                
+                _ => throw new ArgumentOutOfRangeException(),
+            };
         }).CallDeferred();
     }
 }
