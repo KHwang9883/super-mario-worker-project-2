fe59ea48c1f7444900d8554d53700d721e95bf93
GreenSweet233 <2382028511@qq.com>
2025-12-25 16:17:12 +0800

    添加玩家与无敌星状态特殊处理
    在 PlayerSuit 中处理

--- script/level/player/PlayerSuit.cs
+++ script/level/player/PlayerSuit.cs
@@ -34,9 +34,24 @@ public partial class PlayerSuit : Node {
     [Export] public int StarmanTime { get; set; } = 500;
     public int StarmanTimer { get; set; }
 
+    // 玩家光源与无敌星状态光源一共有两个独立的标记
+    private LevelConfig? _levelConfig;
+    [Export] private bool _playerLight;
+    [Export] private bool _starmanLight;
+
     public override void _Ready() {
         Suit = GameManager.PlayerSuitRestore;
         Powerup = GameManager.PlayerPowerupRestore;
+
+        _levelConfig ??= LevelConfigAccess.GetLevelConfig(this);
+        Callable.From(() => {
+            if (_levelConfig.Smwp1LightObjectString[1 - 1] == '1') {
+                _playerLight = true;
+            }
+            if (_levelConfig.Smwp1LightObjectString[61 - 1] == '1') {
+                _starmanLight = true;
+            }
+        }).CallDeferred();
     }
     public override void _PhysicsProcess(double delta) {
         if (GameManager.IsLevelPass) {
@@ -44,8 +59,15 @@ public partial class PlayerSuit : Node {
             GameManager.PlayerPowerupRestore = Powerup;
         }
         
+        // 玩家光源与无敌星状态光源
+        /*if (!_playerLight && !Starman) {
+            _smwpPointLight2D.LightRadius = 0f;
+        }*/
+        
         // 无敌星状态光源半径变化
-        _smwpPointLight2D.LightRadius = Mathf.MoveToward(_smwpPointLight2D.LightRadius, Starman ? 35f : 1f, 0.6f);
+        _smwpPointLight2D.LightRadius =
+            Mathf.MoveToward(_smwpPointLight2D.LightRadius,
+                (_starmanLight && Starman) ? 35f : (_playerLight ? 1f : 0f), 0.6f);
 
         if (!Starman) return;
         
