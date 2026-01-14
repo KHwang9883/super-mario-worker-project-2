62032ff6657fcd21e82f8ea72fcc427d6071b28f
GreenSweet233 <2382028511@qq.com>
2026-01-14 11:15:06 +0800

    Update GeneralVisibleOnScreenEnabler2d.cs

--- script/level/enemy/GeneralVisibleOnScreenEnabler2d.cs
+++ script/level/enemy/GeneralVisibleOnScreenEnabler2d.cs
@@ -5,11 +5,25 @@ public partial class GeneralVisibleOnScreenEnabler2d : VisibleOnScreenNotifier2D
     [Export] public NodePath Path = "..";
     [Export] public bool DisableWhenOutOfScreen;
 
+    private Node? _parent;
+
+    public override void _Ready() {
+        _parent = GetNode(Path);
+    }
+    
     public void OnScreenEntered() {
-        GetNode(Path).ProcessMode = ProcessModeEnum.Inherit;
+        if (_parent == null) {
+            GD.Print("GeneralVisibleOnScreenEnabler2d: _parent is null!");
+            return;
+        }
+        _parent.ProcessMode = ProcessModeEnum.Inherit;
         if (!DisableWhenOutOfScreen) QueueFree();
     }
     public void OnScreenExited() {
-        GetNode(Path).ProcessMode = ProcessModeEnum.Disabled;
+        if (_parent == null) {
+            GD.Print("GeneralVisibleOnScreenEnabler2d: _parent is null!");
+            return;
+        }
+        _parent.ProcessMode = ProcessModeEnum.Disabled;
     }
 }
