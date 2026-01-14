a694355f910d92bb9d39d91211ab7c6aadabf02b
GreenSweet233 <2382028511@qq.com>
2026-01-14 16:28:49 +0800

    修复永动机遇到天花板卡住的问题；修复可穿透平台碰撞箱尺寸不正确的问题

--- script/level/enemy/troopa/shell/PerpetualMotionMachine.cs
+++ script/level/enemy/troopa/shell/PerpetualMotionMachine.cs
@@ -5,6 +5,8 @@ using SMWP.Level.Physics;
 public partial class PerpetualMotionMachine : Node2D {
     [Export] private ShellMovingInteraction? _shellMovingInteraction;
     [Export] private BasicMovement? _basicMovement;
+    
+    [Export] private float _moveUpDistance = 4f;
 
     public override void _PhysicsProcess(double delta) {
         if (_shellMovingInteraction == null) {
@@ -36,15 +38,15 @@ public partial class PerpetualMotionMachine : Node2D {
             _basicMovement.SpeedY = 0f;
             GD.Print($"Collided with PerpetualMotionMachineMarker: {result}");
             var collideResult = shell.MoveAndCollide(
-                new Vector2(_basicMovement.SpeedX, 0f), false, 0.09f);
+                new Vector2(_basicMovement.SpeedX, 0f), true, 0.09f);
             GD.Print($"MoveAndCollide Result: {collideResult}");
             // 循环有限次，防止意外情况
-            for (var i = 0; i < 2400; i++) {
+            for (var i = 0; i < 600; i++) {
                 if (collideResult != null) {
-                    shell.Position += Vector2.Up * 1.01f;
+                    shell.Position += Vector2.Up * _moveUpDistance;
                     shell.ForceUpdateTransform();
                     collideResult = shell.MoveAndCollide(
-                        new Vector2(_basicMovement.SpeedX, 0f), false, 0.09f);
+                        new Vector2(_basicMovement.SpeedX, 0f), true, 0.09f);
                 } else {
                     break;
                 }
