6514c4b7e4274e2a04d96766678e747ff5495a2c
GreenSweet233 <2382028511@qq.com>
2026-01-16 10:56:16 +0800

    调整镜头控制元件的检测区域
    测试关卡及问题：Classic EPIC 5-4 一号挑战区域钻水管后镜头控件不在判定范围内

--- script/level/player/PlayerMovement.cs
+++ script/level/player/PlayerMovement.cs
@@ -922,8 +922,8 @@ public partial class PlayerMovement : Node {
             if (node is not ViewControl viewControl) continue;
             if (_player.Position.X > viewControl.ViewRect.Position.X
                 && _player.Position.X < viewControl.ViewRect.End.X
-                && _player.Position.Y > viewControl.ViewRect.Position.Y
-                && _player.Position.Y < viewControl.ViewRect.End.Y) {
+                && _player.Position.Y > viewControl.ViewRect.Position.Y - 32f
+                && _player.Position.Y < viewControl.ViewRect.End.Y + 32f) {
                 
                 _levelCamera.LimitLeft = (int)viewControl.ViewRect.Position.X;
                 _levelCamera.LimitTop = (int)viewControl.ViewRect.Position.Y;
