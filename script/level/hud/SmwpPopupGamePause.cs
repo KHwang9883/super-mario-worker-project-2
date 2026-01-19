4d93b086c66aab8211dbfcab1da7581f595991ec
GreenSweet233 <2382028511@qq.com>
2026-01-19 12:49:53 +0800

    暂停弹窗改用 Control 节点 Visible 属性实现

---
+++ script/level/hud/SmwpPopupGamePause.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class SmwpPopupGamePause : Control {
+    public void SetVisible() {
+        Visible = true;
+    }
+
+    public void SetInvisible() {
+        Visible = false;
+    }
+}
