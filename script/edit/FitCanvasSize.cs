b40c5d296a11b2f75ba13226e1e156bb715e0364
GreenSweet233 <2382028511@qq.com>
2026-06-14 19:27:43 +0800

    修复Edit拉伸窗口后UI位置不正确的问题

--- script/edit/FitCanvasSize.cs
+++ script/edit/FitCanvasSize.cs
@@ -2,21 +2,15 @@ using Godot;
 
 public partial class FitCanvasSize : Control {
     private CanvasLayer? _targetLayer;
-    private Vector2 _originalSize;
 
     public override void _Ready() {
         _targetLayer = GetParentCanvasLayer();
-        if (_targetLayer != null) {
-            _originalSize = Size;
-        }
     }
 
     public override void _Process(double delta) {
         if (_targetLayer == null) return;
-        Vector2 scale = _targetLayer.Scale;
-        Vector2 newSize = new Vector2(_originalSize.X / scale.X, _originalSize.Y / scale.Y);
-        CustomMinimumSize = newSize;
-        Size = newSize;
+        Vector2 layerScale = _targetLayer.Scale;
+        Scale = new Vector2(1.0f / layerScale.X, 1.0f / layerScale.Y);
     }
 
     private CanvasLayer GetParentCanvasLayer() {
